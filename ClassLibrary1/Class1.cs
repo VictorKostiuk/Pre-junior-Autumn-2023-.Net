f220db760dddf63af665b2e2147e445e210f2809
ViktorKostiuk <vityok.kostiuk@gmail.com>
2023-11-13 21:05:48 +0200

    first lesson task

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary1;
+
+public class Class1 {
+    public static void Main() {   
+        Console.Write("Введіть кількість зошитів: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть ціну одного зошита: ");
+        double notebookPrice = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть кількість ручок: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть ціну однієї ручки: ");
+        double penPrice = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Загальна сума покупки: {(x * notebookPrice) + (y * penPrice)} гривень");
+    }
+}
