fe72be56e94b35a0df6f6a27d4b3d32f02159bc8
ViktorKostiuk <vityok.kostiuk@gmail.com>
2023-11-22 17:46:16 +0200

    third lesson

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -2,26 +2,22 @@
 
 public class Class1 {
     public static void Main() {   
-        Console.Write("Введіть кількість грошей: ");
-        int z = Convert.ToInt32(Console.ReadLine());
-        
-        Console.Write("Введіть кількість зошитів: ");
-        int x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введіть число A: ");
+        int A = int.Parse(Console.ReadLine());
 
-        Console.Write("Введіть ціну одного зошита: ");
-        double notebookPrice = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введіть число B: ");
+        int B = int.Parse(Console.ReadLine());
 
-        Console.Write("Введіть кількість ручок: ");
-        int y = Convert.ToInt32(Console.ReadLine());
+        if (A <= B)
+        {
+            Console.WriteLine("Помилка: A повинно бути більше за B.");
+            return;
+        }
 
-        Console.Write("Введіть ціну однієї ручки: ");
-        double penPrice = Convert.ToDouble(Console.ReadLine());
-
-
-        if (z < x + y) {
-            Console.WriteLine("За наявної кількості грошей ви не зможете здійснити таку покупку");
-        } else {
-            Console.WriteLine($"Загальна сума покупки: {(x * notebookPrice) + (y * penPrice)} гривень");
+        Console.WriteLine("Числа від A до B:");
+        for (int i = A; i >= B; i--)
+        {
+            Console.WriteLine(i);
         }
     }
 }
